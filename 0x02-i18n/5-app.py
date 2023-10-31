aca2748a89f15f473a0adc4e0066e5ca1e444889
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-31 16:32:03 -0700

    Updates

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 """
-This module sets up a Flask app with user mock login and display
+This module sets up a Flask app to emulate a user login and display
 messages based on login status.
 """
 from typing import Any
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from flask_babel import Babel, _, lazy_gettext
 
 app = Flask(__name__)
 babel = Babel(app)
 
-
+# Mock User Table
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-
 }
+
+
+class Config:
+    """
+    Config class to set available languages, default locale,
+    and default timezone for Babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
 
 
 @app.before_request
@@ -32,11 +43,16 @@ def before_request() -> None:
 @app.route('/')
 def index() -> Any:
     """
-    Render the index.html template with login status messages.
+    Render the index.html template based on the user's login status.
     """
-    return render_template('5-index.html', logged_in_as=lazy_gettext('You
-                                                                      are logged in as %(username)s.'),
-                           not_logged_in=lazy_gettext('You are not logged in.'))
+    return render_template('5-index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route('/logout')
+def logout() -> Any:
+    """
+    Simulates a logout by setting g.user to None and redirecting
+    to the home page.
+    """
+    g.user = None
+    return render_template('5-index.html')
