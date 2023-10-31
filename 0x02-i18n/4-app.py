962998c1beebcf1f9e55fa5409fe1265d2a654b5
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-31 11:50:03 -0700

    Updates

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -3,42 +3,41 @@
 This module sets up a Flask app with Flask-Babel extension and
 supports forcing a particular locale.
 """
-from typing import Any
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
+from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
     """
     Config class to set available languages and default
     locale/timezone for Babel.
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 
 
+@app.route('/')
+def index_page():
+    """
+    Render the index.html template with internationalized text.
+    """
+    return render_template('4-index.html')
+
+
 @babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """
     Determine the best-matching language based on request.accept_languages
     or forced locale.
     """
-    forced_locale = request.args.get('locale')
-    if forced_locale and forced_locale in app.config['LANGUAGES']:
-        return forced_locale
+    requested_locale = request.args.get('locale')
+
+    if requested_locale and requested_locale in Config.LANGUAGES:
+        return requested_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index() -> Any:
-    """
-    Render the index.html template with internationalized text.
-    """
-    return render_template('4-index.html', home_title=_('home_title'),
-                                                       home_header=_('home_header'))
